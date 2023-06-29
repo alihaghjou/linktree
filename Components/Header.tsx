ba25f0766a3c1086545d8efd0b2cd82d40d1a064
alihaghjou <alihaghjou30@gmail.com>
2023-06-29 09:48:58 +0330

    header completed and icon added

--- Components/Header.tsx
+++ Components/Header.tsx
@@ -1,7 +1,15 @@
+import Image from "next/image";
+import Link from "next/link";
+import icon from "../public/icon.png"
+
 const Header = () => {
   return (
-    <header className="border-b-2 border-b-slate-800 p-2 m-1 text-center text-xl">
-      header
+    <header className="border-b-2 border-b-slate-800 p-2 m-1 text-center text-xl flex items-center justify-between">
+      <Image src={icon} alt="icon" width={64} height={64} className="rounded-full" />
+      <div className="flex gap-4 items-center">
+        <Link href={"/"}>Home</Link>
+        <Link href={"/about"}>About</Link>
+      </div>
     </header>
   );
 };
