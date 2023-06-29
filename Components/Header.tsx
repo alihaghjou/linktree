d5543af0d6059507408a0d12237bac6f753c1812
alihaghjou <alihaghjou30@gmail.com>
2023-06-29 11:34:32 +0330

    trying to fix production style bug

--- Components/Header.tsx
+++ Components/Header.tsx
@@ -4,7 +4,7 @@ import icon from "../public/icon.png"
 
 const Header = () => {
   return (
-    <header className="border-b-2 border-b-slate-800 p-2 m-1 text-center md:text-xl flex items-center justify-between">
+    <header className="header">
       <Image src={icon} alt="icon" width={64} height={64} className="rounded-full" />
       <div className="flex gap-4 items-center">
         <Link href={"/"} className="hover:text-gray-600">Home</Link>
